3a24572afd24c6b27111e4eb9b705983d09813cf
codeHasib <mohammadhasib9827@gmail.com>
2026-05-17 21:27:20 +0600

    setup initialized

---
+++ index.js
@@ -0,0 +1,42 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const app = express();
+// Middlewares
+app.use(cors());
+app.use(express.json());
+// SERVER-PORT
+const SERVER_PORT = 5000;
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@cluster0.cmdrutm.mongodb.net/?appName=Cluster0`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB! yey",
+    );
+    const db = client.db("rescume");
+    const petsCollection = db.collection("pets");
+    app.get("/pets", async (req, res) => {
+      const cursor = petsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.listen(SERVER_PORT, console.log(`${SERVER_PORT} is listening on port`));
