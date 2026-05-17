e8931accf30ae4ab32761672627d5d784e9eab2f
codeHasib <mohammadhasib9827@gmail.com>
2026-05-17 23:51:30 +0600

    add requests apis

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
-// Middlewares
+
 app.use(cors());
 app.use(express.json());
-// SERVER-PORT
+
 const SERVER_PORT = 5000;
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@cluster0.cmdrutm.mongodb.net/?appName=Cluster0`;
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -31,13 +31,19 @@ const verifyToken = (req, res, next) => {
     return res.status(401).json({ message: "Unauthorized" });
   }
 
-  next();
+  try {
+    req.user = { email: token };
+    next();
+  } catch (error) {
+    return res.status(403).json({ message: "Forbidden" });
+  }
 };
 
 async function run() {
   try {
     const db = client.db("rescume");
     const petsCollection = db.collection("pets");
+    const requestCollection = db.collection("requests");
 
     app.get("/pets", async (req, res) => {
       const cursor = petsCollection.find();
@@ -45,27 +51,85 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/pets/:id", verifyToken, async (req, res) => {
+      const query = {
+        _id: new ObjectId(req.params.id),
+      };
+      const result = await petsCollection.findOne(query);
+      res.send(result);
+    });
+
     app.post("/pets", verifyToken, async (req, res) => {
       const newPet = req.body;
+      newPet.ownerEmail = req.user.email;
       const result = await petsCollection.insertOne(newPet);
       res.send(result);
     });
 
-    app.delete("/pets/:id", async (req, res) => {
+    app.delete("/pets/:id", verifyToken, async (req, res) => {
       const query = {
         _id: new ObjectId(req.params.id),
+        ownerEmail: req.user.email,
       };
       const result = await petsCollection.deleteOne(query);
       res.send(result);
     });
 
-    app.patch("/pets/:id", async (req, res) => {
+    app.patch("/pets/:id", verifyToken, async (req, res) => {
       const query = {
         _id: new ObjectId(req.params.id),
+        ownerEmail: req.user.email,
       };
       const updatedPet = req.body;
       const result = await petsCollection.updateOne(query, {
         $set: updatedPet,
+      });
+      res.send(result);
+    });
+
+    app.get("/requests", verifyToken, async (req, res) => {
+      const cursor = requestCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.post("/requests", verifyToken, async (req, res) => {
+      const newRequest = req.body;
+
+      const petQuery = { _id: new ObjectId(newRequest.petId) };
+      const targetPet = await petsCollection.findOne(petQuery);
+
+      if (!targetPet) {
+        return res.status(404).json({ message: "Pet not found" });
+      }
+
+      if (targetPet.ownerEmail === req.user.email) {
+        return res
+          .status(403)
+          .json({ message: "You cannot adopt your own listed pet" });
+      }
+
+      newRequest.applicantEmail = req.user.email;
+      const result = await requestCollection.insertOne(newRequest);
+      res.send(result);
+    });
+
+    app.delete("/requests/:id", verifyToken, async (req, res) => {
+      const query = {
+        _id: new ObjectId(req.params.id),
+        applicantEmail: req.user.email,
+      };
+      const result = await requestCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.patch("/requests/:id", verifyToken, async (req, res) => {
+      const query = {
+        _id: new ObjectId(req.params.id),
+      };
+      const updatedRequest = req.body;
+      const result = await requestCollection.updateOne(query, {
+        $set: updatedRequest,
       });
       res.send(result);
     });
